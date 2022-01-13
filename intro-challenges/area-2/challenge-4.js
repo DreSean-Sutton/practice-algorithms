474ea570bcf59a32e3724473363dcf4899ec2e7d
DreSean-Sutton <dreseansutton@gmail.com>
2022-01-13 15:36:33 -0800

    Finished challenge-4

---
+++ intro-challenges/area-2/challenge-4.js
@@ -0,0 +1,10 @@
+function solution(inputArray) {
+  let largestProduct = -999;
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    let twoNumbers = inputArray[i] * inputArray[i + 1]
+    if (twoNumbers > largestProduct) {
+      largestProduct = twoNumbers;
+    }
+  }
+  return largestProduct;
+}
