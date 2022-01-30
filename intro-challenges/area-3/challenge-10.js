01a3081d22294e2754594a79a59f964d2d19409a
DreSean-Sutton <dreseansutton@gmail.com>
2022-01-30 14:01:18 -0800

    Finished challenge-10

--- intro-challenges/area-3/challenge-10.js
+++ intro-challenges/area-3/challenge-10.js
@@ -12,13 +12,13 @@
     a. assigns the value of 0 to i
     b. execute code block if i is less than lowestLength.length
     c. i++
-    -if greatestLength[i].includes lowestLength[i]
-        commonLetter++
+    -if greatestLength.includes lowestLength[i]
+      call the replace method of greatestLength at lowestLength[i] with an empty string and assign that value to greatestLength
+      commonLetter++
 return commonLetter
 */
 
 function solution(s1, s2) {
-  debugger;
   let commonLetter = 0;
   let lowestLength;
   let greatestLength;
@@ -26,15 +26,27 @@ function solution(s1, s2) {
     lowestLength = s2.slice(0)
     greatestLength = s1.slice(0)
   } else {
-      lowestLength = s1.slice(0)
-      greatestLength = s2.slice(0)
+    lowestLength = s1.slice(0)
+    greatestLength = s2.slice(0)
   }
   for (let i = 0; i < lowestLength.length; i++) {
-    if (lowestLength[i].includes(greatestLength[i])) {
+    if (greatestLength.includes(lowestLength[i])) {
+      greatestLength = greatestLength.replace(lowestLength[i], '');
       commonLetter++
     }
   }
   return commonLetter;
 }
 
-solution('abcde', 'abegs');
+// BETTER SOLUTION BELOW. FINDING THE GREATEST/LOWEST LENGTH WASN'T NEEDED
+
+function solution(s1, s2) {
+  let count = 0;
+  for (let i = 0; i < s1.length; i++) {
+    if (s2.indexOf(s1[i]) != -1) {
+      count++;
+      s2 = s2.replace(s1[i], '');
+    }
+  }
+  return count;
+}
