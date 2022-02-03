8e05ace043fbe3a7297b20e8dcb35e33886b80ae
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-03 13:35:16 -0800

    Currently doing pseudocode for challenge-16

---
+++ intro-challenges/area-4/challenge-16.js
@@ -0,0 +1,16 @@
+/*
+-check to see if both arrays contain the same exact numbers
+-create a counter that increments if both arrays at the same index aren't equal
+-iterate through the array, checking to see if it's strictly equal
+-return false if counter is higher than 2
+-return true if iteration ends and counter is at or less than 2
+/*
+
+/*
+-create variable named wrongIndex and assign it the value of 0
+-create a for loop that:
+  a. assigns the value of 0 to i
+  b. executes code block if i is less than arr1.length
+  c. i++
+  -
+/*
