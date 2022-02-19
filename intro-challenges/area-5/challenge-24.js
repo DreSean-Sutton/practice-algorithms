59905142bca7a4f1ccd48c38d39ac87858d7281c
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-18 23:30:29 -0800

    Finished challenge-24

--- intro-challenges/area-5/challenge-24.js
+++ intro-challenges/area-5/challenge-24.js
@@ -1,22 +1,57 @@
+/*
+-create a variable named newMatrix and assign it the value of an emptry array literal
+-create a variable named mineCounter and assign it the value of 0
+-create a variable named rowLoopStopper and assign it the value of 2
+-create a for loop that:
+a. assigns the value of 0 to i
+b. executes code block if i is less than the length property of matrix
+c. i++
+  -create a variable named colLoopStopper and assign it the value of 2
+  -create a variable named tempMatrix and assign it the value of an empty array literal
+  -create a for loop that:
+  a. assigns the value of 0 to j
+  b. executes code block if j is less than the length property of matrix at index i
+  c. j++
+    -create a for loop that:
+    a. assigns the value of i - 1 to k
+    b. executes code block if k is less than rowLoopStopper
+    c. k++
+      -create a for loop that:
+      a. assigns the value of j - 1 to l
+      b. executes code block if l is less than colLoopStopper
+      c. l++
+        -if matrix at index k is strictly equal to undefined or
+        matrix at index k at index l is strictly equal to unefined:
+          call the continue statement
+        -if i is strictly equal to k and
+        j is strictly equal to l
+          call the continue statement
+        -if matrix at index k at index l is true:
+          increment mineCounter by 1
+    -call the push method of tempMatrix with mineCounter as it's argument
+    -assign the value of 0 to mineCounter
+    -increment colLoopStopper by 1
+  -call the push method of newMatrix with tempMatrix as it's argument
+  -increment rowLoopStopper by 1
+-return newMatrix
+*/
+
 function solution(matrix) {
-  debugger;
   let newMatrix = [];
   let mineCounter = 0;
-  let checkCurrentIIndex = 0;
-  let checkCurrentJIndex = 0;
+  let rowLoopStopper = 2;
   for (let i = 0; i < matrix.length; i++) {
-    let rowLoopStopper = 2;
     let colLoopStopper = 2;
     let tempMatrix = [];
     for (let j = 0; j < matrix[i].length; j++) {
       for (let k = i - 1; k < rowLoopStopper; k++) {
         for (let l = j - 1; l < colLoopStopper; l++) {
           if(matrix[k] === undefined ||
-          matrix[k][l] === undefined) {
-            continue;
-          }
-          if (i + checkCurrentIIndex === k &&
-          j + checkCurrentJIndex === l) {
+            matrix[k][l] === undefined) {
+              continue;
+            }
+          if (i === k &&
+            j === l) {
               continue;
             }
           if (matrix[k][l]) {
@@ -27,19 +62,15 @@ function solution(matrix) {
       tempMatrix.push(mineCounter);
       mineCounter = 0;
       colLoopStopper++;
-      checkCurrentJIndex++
     }
     newMatrix.push(tempMatrix);
-    colLoopStopper = 0
     rowLoopStopper++;
-    checkCurrentIIndex++
-    checkCurrentJIndex = 0
   }
   return newMatrix;
 }
 
 solution(
-[[true, false, false],
-[false, true, false],
-[false, false, false]]
+[[true, false, false, true],
+[false, false, true, false],
+[true, true, false, true]]
 )
