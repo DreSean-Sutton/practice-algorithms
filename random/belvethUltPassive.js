f86c6efb01312366006077258cf813fc4dfbcb4b
DreSean-Sutton <dreseansutton@gmail.com>
2022-06-25 18:01:27 -0700

    Randomly decided to build an algorithm for calculating Bel'veth's damage from her ultimate's passive

---
+++ random/belvethUltPassive.js
@@ -0,0 +1,18 @@
+function belvethUltPassive(baseDmg, hits, bonusAd) {
+  let totalDmg = 0;
+  let i = 0;
+  bonusAd
+    ? fullDmg = baseDmg + Math.round(bonusAd / 100)
+    : fullDmg = baseDmg
+  while (i <= hits) {
+    if (i % 2 === 0) {
+      damagePer2Hits(fullDmg, i);
+    }
+    i++;
+  }
+  function damagePer2Hits(dmg, stack) {
+    totalDmg += dmg * stack / 2;
+    return;
+  }
+  return totalDmg
+}
