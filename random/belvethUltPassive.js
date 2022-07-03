f303fe98f30795254ed2735927f1073929022cc7
DreSean-Sutton <dreseansutton@gmail.com>
2022-07-02 21:02:15 -0700

    Altered belveth algo slightly to account for single decimal point rounding on the return value

--- random/belvethUltPassive.js
+++ random/belvethUltPassive.js
@@ -2,7 +2,7 @@ function belvethUltPassive(baseDmg, hits, bonusAd) {
   let totalDmg = 0;
   let i = 0;
   bonusAd
-    ? fullDmg = baseDmg + Math.fround(bonusAd / 100)
+    ? fullDmg = (baseDmg + bonusAd / 100)
     : fullDmg = baseDmg
   while (i <= hits) {
     if (i % 2 === 0) {
@@ -14,5 +14,5 @@ function belvethUltPassive(baseDmg, hits, bonusAd) {
     totalDmg += dmg * stack / 2;
     return;
   }
-  return totalDmg
+  return Number(totalDmg.toFixed(1));
 }
