75afaeab6e989e0622426ddf0a1d5c725c88f9d5
DreSean-Sutton <dreseansutton@gmail.com>
2022-07-06 03:03:36 -0700

    Fixed algorithm by getting rid of the bonusAD approximation

--- random/belvethUltPassive.js
+++ random/belvethUltPassive.js
@@ -1,8 +1,8 @@
-function belvethUltPassive(baseDmg, hits, bonusAd) {
+function belvethUltPassive(baseDmg, hits, bonusAD) {
   let totalDmg = 0;
   let i = 0;
-  bonusAd
-    ? fullDmg = (baseDmg + bonusAd / 100)
+  bonusAD
+    ? fullDmg = baseDmg + (baseDmg * 0.0012 * bonusAD)
     : fullDmg = baseDmg
   while (i <= hits) {
     if (i % 2 === 0) {
@@ -16,3 +16,22 @@ function belvethUltPassive(baseDmg, hits, bonusAd) {
   }
   return Number(totalDmg.toFixed(1));
 }
+
+// Another way on how to do it below
+
+// const belvethUltPassive = (baseDmg, hits, bonusAD) => {
+//   let burst = 0;
+//   let damage = 0;
+//   for (let hit = 1; hit <= hits; hit++) {
+//     // every two hits, do a burst of damage
+//     if (hit % 2 === 0) {
+//       // each burst increases the damage of the next burst by the amount just dealt
+//       // burst damage is base damage plus an additional 0.12% of base damage per bonus ad
+//       bonusAD
+//         ? burst += baseDmg + (baseDmg * 0.0012 * bonusAD)
+//         : burst += baseDmg
+//       damage += burst;
+//     }
+//   }
+//   return +(damage).toFixed(1);
+// };
