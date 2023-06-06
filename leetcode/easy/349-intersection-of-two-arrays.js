3a2e95e76f9a74acf2e93b19ed8f0f61464bfab1
DreSean-Sutton <dreseansutton@gmail.com>
2023-06-05 20:34:40 -0700

    Updated an old solution to a far superior one

--- leetcode/easy/349-intersection-of-two-arrays.js
+++ leetcode/easy/349-intersection-of-two-arrays.js
@@ -3,6 +3,20 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+// Second better solution
+var intersection = function (nums1, nums2) {
+  const resultSet = new Set();
+  const nums1Set = new Set([...nums1]);
+  nums2.forEach(e => {
+    if (nums1Set.has(e)) {
+      resultSet.add(e);
+    }
+  })
+  return [...resultSet];
+};
+
+// First solution
 var intersection = function (nums1, nums2) {
   const result = [];
   nums1.forEach(elem => {
